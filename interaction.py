2331c804df9b7ae6a50323da03949eb3abc31464
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-07-06 01:30:56 +0200

    Initial commit

--- interaction.py
+++ interaction.py
@@ -2,8 +2,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 chrome_driver_path = r"C:\Users\user\Downloads\chromedriver_win32\chromedriver.exe"
 chrome_options = Options()

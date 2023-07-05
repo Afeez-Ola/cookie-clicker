5ed76c2f725d00183cff56b0c65fb1072322366f
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-07-06 01:30:41 +0200

    Initial commit

--- interaction.py
+++ interaction.py
@@ -1,9 +1,7 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -12,18 +10,6 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
-
-closest_value = None
-
-
-def cookie_clicking(cookie):
-    cookie.click()
-
-
-cookie = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, "cookie"))
-)
-clicks_per_second = 100
 
 items_dict = {
     "buyCursor": 15,
@@ -35,33 +21,45 @@ items_dict = {
     "buyPortal": 1000000,
     "buyTime": 123456789
 }
-items = [item for item in items_dict.keys()]
+
+clicks_per_second = 100
 
 
-def closest_item():
-    global closest_value
+def cookie_clicking():
+    cookie = driver.find_element(By.ID, "cookie")
+    cookie.click()
+
+
+def get_cookie_count():
+    cookie_count_str = driver.find_element(By.ID, "money").text.replace(",", "")
+    return int(cookie_count_str)
+
+
+def get_closest_item(cookie_count):
+    closest_value = None
     for value in items_dict.values():
         if value <= cookie_count:
             if closest_value is None or value > closest_value:
                 closest_value = value
-    if closest_value is not None:
-        for key, value in items_dict.items():
-            if value == closest_value:
-                return closest_value
+    return closest_value
 
 
-# for price in items_dict.values():
-#     print(price)
+def buy_item(item):
+    item_to_buy = driver.find_element(By.CSS_SELECTOR, f"#{item} > b")
+    item_to_buy.click()
+
 
 while True:
     for _ in range(clicks_per_second):
-        cookie_clicking(cookie)
-        cookie_count = int((driver.find_element(By.ID, "money")).text.replace(",", ""))
-    print(cookie_count)
-    keys = [key for key, val in items_dict.items() if val == closest_item()]
-    item_to_buy = (driver.find_element(By.CSS_SELECTOR, f"#{keys[0]} > b"))
-    item_to_buy.click()
-    print(keys[0])
+        cookie_clicking()
+    cookie_count = get_cookie_count()
+    print("Cookie count:", cookie_count)
+    closest_value = get_closest_item(cookie_count)
+    if closest_value is not None:
+        keys = [key for key, value in items_dict.items() if value == closest_value]
+        item_to_buy_key = keys[0]
+        buy_item(item_to_buy_key)
+        print("Buying item:", item_to_buy_key)
     time.sleep(5)
-    print("this is after 5 seconds")
-    clicks_per_second = clicks_per_second + 50
+    print("This is after 5 seconds")
+    clicks_per_second += 50
